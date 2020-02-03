c33d1b3f5281a6d32e72744a4a26b18b4bccec34
ScottSummerford <scottsummerford@simventions.com>
2020-02-03 11:40:02 -0500

    Updated script. Reverted git-ignore because we actually want it to track

--- web-app/Scripts/deploy_dexter_conversion.bat
+++ web-app/Scripts/deploy_dexter_conversion.bat
@@ -5,7 +5,7 @@ cd web-transform\
 del /q *
 cd ..
 copy web-app\Uploads\%1 web-transform\
-rename web-transform\%1 conversion.java
+rename web-transform\%1 transform.java
 git add .
 git commit -m "AUTO-GENERATED - Deploying new dexter conversion"
 git push
