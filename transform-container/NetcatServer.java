715296d94d34d87f79fa9cc9260a1632421d21ca
ScottSummerford <scottsummerford@simventions.com>
2020-02-06 13:39:14 -0500

    Converted receive container to java server and updated to use shell scripts

--- transform-container/NetcatServer.java
+++ transform-container/NetcatServer.java
@@ -4,17 +4,14 @@ import static java.lang.System.exit;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import sun.misc.IOUtils;
 
 /**
  * Modified server code for Dexter demo
  */
-public class NetcatServer
-{
+public class NetcatServer {
 
   private static Transform dexterTransformer;
   private static Scanner scanner;
@@ -22,15 +19,13 @@ public class NetcatServer
   private static InputStream serverInput;
   private static OutputStream serverOuput;
   private static PrintWriter serverWriter;
-  private static int PORT = 9991;
+  private static final int PORT = 9991;
 
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) {
     initializeServer();
   }
 
-  public static void initializeServer()
-  {
+  public static void initializeServer() {
     try (ServerSocket serverSocket = new ServerSocket(PORT)) {
       connectionSocket = serverSocket.accept();
 
@@ -43,19 +38,19 @@ public class NetcatServer
       dexterTransformer = new Transform();
 
       serverWriter.println("Server Initialized. Enter Stop to terminate.");
-      serverWriter.println("Please provide " + dexterTransformer.numInputParameters + " numeric arguments delimited with a space.");
+      serverWriter.println(
+          "Please provide " + dexterTransformer.numInputParameters + " numeric arguments delimited with a space.");
 
       while (scanner.hasNextLine()) {
-        readServerInput();
+        double[] output = readServerInput();
+        sendToReadContainer(output);
       }
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
-  private static void readServerInput()
-  {
+  private static double[] readServerInput() {
     String line = scanner.nextLine();
 
     if (line.toLowerCase().trim().equals("stop")) {
@@ -63,21 +58,19 @@ public class NetcatServer
       exit(0);
     }
 
-    sendToReadContainer();
-
     String[] values = line.split(" ");
     double[] output = transformInput(values);
 
-    //Todo - netcat in and send it to the party. For now, just log.
+    // Todo - netcat in and send it to the party. For now, just log.
     for (double convertedValue : output) {
       serverWriter.println(convertedValue);
     }
 
+    return output;
   }
 
-  private static double[] transformInput(String[] values)
-  {
-    ArrayList<Double> parsedValues = new ArrayList<Double>();
+  private static double[] transformInput(String[] values) {
+    ArrayList<Double> parsedValues = new ArrayList<>();
     double[] output = new double[dexterTransformer.numOutputParameters];
 
     if (values.length == dexterTransformer.numInputParameters) {
@@ -85,31 +78,27 @@ public class NetcatServer
         parsedValues.add(Double.parseDouble(value));
       }
 
-      dexterTransformer.convert(parsedValues.get(0).doubleValue(),
-        parsedValues.get(1).doubleValue(), parsedValues.get(2).doubleValue(), output);
+      dexterTransformer.convert(parsedValues.get(0), parsedValues.get(1), parsedValues.get(2), output);
     }
 
     return output;
   }
 
-  private static void sendToReadContainer()
-  {
+  private static void sendToReadContainer(double[] output) {
     serverWriter.println("Transform complete. Executing transfer to linked node.");
 
-    boolean isWindows = System.getProperty("os.name")
-      .toLowerCase().startsWith("windows");
+    String outputLine = output.toString();
+    boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
 
     try {
-      String homeDirectory = System.getProperty("user.home");
       Process process;
+      String[] commange = { "sh", "/usr/app/SendNetcatMessage.bat" };
       if (isWindows) {
-        //I don't think we're going to get far if this shows up as windows.
-        process = Runtime.getRuntime()
-          .exec(String.format("cmd.exe /c dir %s", homeDirectory));
-      }
-      else {
-        process = Runtime.getRuntime()
-          .exec(new String[]{"nc", "0.0.0.0", "8888"});
+        // I don't think we're going to get far if this shows up as windows.
+        serverWriter.println("Cannot use netcat with Windows. Please change the containers OS and try again.");
+        return;
+      } else {
+        process = Runtime.getRuntime().exec(commange);
       }
 
       BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -118,19 +107,17 @@ public class NetcatServer
       while ((line = reader.readLine()) != null) {
         serverWriter.println(line);
       }
-      
+
       debugErrorStatements(process);
 
       int exitCode = process.waitFor();
       assert exitCode == 0;
-    }
-    catch (Exception ex) {
+    } catch (Exception ex) {
       serverWriter.println(ex.getMessage());
     }
   }
 
-  private static void debugErrorStatements(Process process)
-  {
+  private static void debugErrorStatements(Process process) {
     BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
     String line;
 
@@ -138,12 +125,9 @@ public class NetcatServer
       while ((line = reader.readLine()) != null) {
         serverWriter.println(line);
       }
-    }
-    catch (IOException ex) {
+    } catch (IOException ex) {
       Logger.getLogger(NetcatServer.class.getName()).log(Level.SEVERE, null, ex);
     }
-    
-    
 
   }
 }
